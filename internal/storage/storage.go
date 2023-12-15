36b95c5c9b9f89cb9ff7bf9287637ed95bbff1f4
AndreyVLZ <31869969+AndreyVLZ@users.noreply.github.com>
2023-12-15 15:56:09 +0300

    Revert "Добавить agent"

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -2,16 +2,4 @@ package storage
 
 type Storage interface {
 	Set(name, typeStr, valStr string) error
-	//Repo(mType metric.MetricType) (Repository, error)
-	GaugeRepo() Repository
-	CounterRepo() Repository
-}
-
-type Repository interface {
-	Set(name, valStr string) error
-	Get(string) (string, error)
-	List() map[string]string
-
-	// NOTE: Delete
-	Range()
 }
